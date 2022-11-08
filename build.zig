a32b35dacba3c6a7d2acc21709997bc6e161e57c
Coleman Broaddus <coleman.broaddus@gmail.com>
2022-11-08 13:50:07 -0500

    LINK ERROR.
    ~/Desktop/biopencil> zig build
    MachO Flush... warning(link): found dylib is incompatible with the required minimum version
    warning(link):   dylib: /System/Library/Frameworks/CFNetwork.framework/Versions/A/CFNetwork
    warning(link):   required minimum version: 65536
    warning(link):   dylib version: 0
    error(link): unhandled symbol type: absolute
    error(link):   symbol 'F_0_344'
    error(link):   first definition in '/usr/local/Cellar/jpeg-turbo/2.1.4/lib/libjpeg.a(jdcolor-avx2.asm.o)'
    error: UnhandledSymbolType
    error: biopencil...
    error: The following command exited with error code 1:

--- build.zig
+++ build.zig
@@ -13,8 +13,37 @@ pub fn build(b: *Builder) void {
         exe.addIncludeDir("/usr/local/include/"); // tiff.h
         exe.addIncludeDir("libs/include/"); // CL/opencl.h
         exe.linkFramework("OpenCL");
-        exe.linkSystemLibrary("tiff");
-        exe.linkSystemLibrary("SDL2");
+        // exe.linkSystemLibrary("tiff");
+        // exe.linkSystemLibrary("SDL2");
+
+        exe.addIncludeDir("/usr/local/include/SDL2/");
+        exe.addObjectFile("/usr/local/lib/libSDL2.a");
+        exe.addObjectFile("/usr/local/lib/libtiff.a");
+
+
+        // TIFF transitive
+        exe.addObjectFile("/usr/local/lib/libjpeg.a");
+
+        // SDL transitive
+        exe.linkSystemLibrary("iconv");
+        exe.linkFramework("AppKit");
+        exe.linkFramework("AudioToolbox");
+        exe.linkFramework("Carbon");
+        exe.linkFramework("Cocoa");
+        exe.linkFramework("CoreAudio");
+        exe.linkFramework("CoreFoundation");
+        exe.linkFramework("CoreGraphics");
+        exe.linkFramework("CoreHaptics");
+        exe.linkFramework("CoreVideo");
+        exe.linkFramework("ForceFeedback");
+        exe.linkFramework("GameController");
+        exe.linkFramework("IOKit");
+        exe.linkFramework("Metal");
+
+
+
+
+
         exe.install();
     }
 
